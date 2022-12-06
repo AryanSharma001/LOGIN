0e3a16a5462c2a6ac2e79ae2cba4bdec911d0a4e
AryanSharma001 <110148251+AryanSharma001@users.noreply.github.com>
2022-12-06 23:38:23 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
   const express = require("express");
   const app = express();
+  app.use(express.json());
 
   app.listen(5000,()=>{
     console.log("server started");
-  })
+  });
+
+  app.post("/post",async(req,res)=>{
+    console.log(req.body);
+  });
